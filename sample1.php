6a50f259092fcd9b052b3409e9b169ebca4cb830
Muhamad Hanafiah Yahya <ibnuyahya@gmail.com>
2015-03-25 10:31:14 +0800

    Update sample1.php

--- sample1.php
+++ sample1.php
@@ -1,20 +1,3 @@
 <?php
 
-//edit by hanafiah 
-function test(){
-	echo 'hello';
-}
-
-function test2(){
-	echo 'zul & hanafiah';
-}
-
-function test4(){
-	echo 'farah;
-}
-
-function test5(){
-	echo 'test54568';
-}
-
-?>
+phpinfo();
